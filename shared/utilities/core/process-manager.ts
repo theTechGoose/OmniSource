3d20a708c56675f429af26815e72010c888c7b21
Rofer11607 <rafac@monsterrg.com>
2024-11-27 22:36:58 -0500

    a

--- shared/utilities/core/process-manager.ts
+++ shared/utilities/core/process-manager.ts
@@ -1,30 +1,31 @@
-import {runCommand, withTryCatch} from "@shared/utils";
+import { runCommand, withTryCatch } from "@shared/utils";
 
 export class ProcessManager {
-  processes = [] as Array<Deno.ChildProcess>
-  constructor(private root: string) {}
+  processes = [] as Array<Deno.ChildProcess>;
+  constructor(private root: string) {
+    Deno.addSignalListener("SIGTERM", () => {
+      this.killAll();
+    });
+  }
 
   killAll() {
-    for(const p of this.processes) {
-
-
-      withTryCatch(() =>p.kill())
+    for (const p of this.processes) {
+      withTryCatch(() => p.kill("SIGTERM"));
     }
   }
 
   spawn(_cmd: string) {
-    const cmd = _cmd.split(' ')
-    const p = runCommand(this.root, cmd)
-    this.processes.push(p)
-    return p
+    const cmd = _cmd.split(" ");
+    const p = runCommand(this.root, cmd);
+    this.processes.push(p);
+    return p;
   }
-  
 
-   handleChange(cmds: string[], comment?: string) {
-    if(comment) console.log(comment)
-    this.killAll()
-    for(const _cmd of cmds) {
-      this.spawn(_cmd)
+  handleChange(cmds: string[], comment?: string) {
+    if (comment) console.log(comment);
+    this.killAll();
+    for (const _cmd of cmds) {
+      this.spawn(_cmd);
     }
   }
 }
