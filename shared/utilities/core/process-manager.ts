ac72813ba3b9873be61a63d150a469ca1a3d018c
Rofer11607 <rafac@monsterrg.com>
2024-11-27 22:44:17 -0500

    added debouncing

--- shared/utilities/core/process-manager.ts
+++ shared/utilities/core/process-manager.ts
@@ -1,11 +1,12 @@
+import {debounce} from "#std";
 import { runCommand, withTryCatch } from "@shared/utils";
 
 export class ProcessManager {
   processes = [] as Array<Deno.ChildProcess>;
+  debouncer = {} as any;
   constructor(private root: string) {
     Deno.addSignalListener("SIGTERM", () => {
       this.killAll();
-
     });
   }
 
@@ -16,10 +17,24 @@ export class ProcessManager {
   }
 
   spawn(_cmd: string) {
+    this.debounce(_cmd);
+  }
+
+  private _spawn(_cmd: string) {
     const cmd = _cmd.split(" ");
     const p = runCommand(this.root, cmd);
     this.processes.push(p);
     return p;
+  }
+
+  private debounce(cmd: string) {
+    const debouncer = this.debouncer[cmd];
+    if(debouncer) debouncer();
+    const newDebouncer = debounce(() => {
+      this._spawn(cmd);
+    }, 3000);
+    this.debouncer[cmd] = newDebouncer;
+    newDebouncer();
   }
 
   handleChange(cmds: string[], comment?: string) {
