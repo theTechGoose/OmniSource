29d84fa0759afce4518d7b8d278ef5066df15ed3
Raphael Castro <goose@Raphaels-MBP.lan>
2024-12-06 11:58:49 -0500

    pulling base from diff commit.

--- depricated/core/resolution/core/_mod.ts
+++ depricated/core/resolution/core/_mod.ts
@@ -5,7 +5,7 @@ import { Reflect } from "#reflect";
 export function Dependency<T>(target: T) {
   const params = Reflect.getMetadata("design:paramtypes", target) ?? [];
   const dep = new PreparedDependency(target as any, params);
-  λ.vault.manifest.addDependency(dep)
+  λ.vault.manifest.addDependency(dep);
   return class {
     static id: string = dep.id;
     id: string = dep.id;
